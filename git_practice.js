1fbe35fa932efcce481283593ddb35e581ec6349
Justin Duong <jduong@umich.edu>
2022-01-11 12:55:13 -0500

    Init subBranch_1.

--- git_practice.js
+++ git_practice.js
@@ -1,2 +1,3 @@
 console.log('file for main branch.');
 console.log('hello I added this statement on a branch that will be connected to main.');
+console.log('hello I made subBranch_1, which is a branch off of branch_1.');
