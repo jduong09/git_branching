9a95f15a0a0f0e56ea09240ff7c27c893a0f759a
Justin Duong <jduong@umich.edu>
2022-01-11 12:59:01 -0500

    Init subBranch_2.

--- git_practice.js
+++ git_practice.js
@@ -1,2 +1,3 @@
 console.log('file for main branch.');
 console.log('hello I added this statement on a branch that will be connected to main.');
+console.log('hello I added a subBranch_2, which should not have any info from subBranch_1.');
